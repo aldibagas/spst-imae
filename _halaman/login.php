4718e16ff2d823b6a8f3f162e91f00b31d0dddee
izzuddin3 <91045905+izzuddin3@users.noreply.github.com>
2021-10-09 10:39:59 +0700

    Update login.php

--- _halaman/login.php
+++ _halaman/login.php
@@ -1,11 +1,47 @@
-<?php
-   $title="Masuk";
-   $Template=false;
-?>
-<html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+$title="masuk";
+$Template=false;
+
+	$servername = "localhost";
+
+	/* nama database kita */
+    $database = "spst_user"; 
+
+    /* nama user yang terdaftar pada database (default: root) */
+    $username = "root";
+
+    /* password yang terdaftar pada database (default : "") */ 
+    $password = ""; 
+
+    // membuat koneksi
+    $conn = mysqli_connect($servername, $username, $password, $database);
+	if(isset($_POST['kirim'])){
+		$user = $_POST['username'];
+		$pass = $_POST['pass'];
+	
+		$sql = "SELECT * FROM `tb_pengguna` WHERE `Nama` = '$user' and `Kata Sandi` = '$pass'";
+		$run = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($run);
+	
+		if($row > 0){
+			session_start();
+			$_SESSION['nama'] = $row['Nama'];
+			header('location:index.php?halaman=beranda');
+		}else{
+			echo'
+			<script type="text/JavaScript">
+				var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+				myModal.show();
+			</script>';
+		}
+	}
+
+?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">	
 <title>Masuk</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
 <style>
 	.login{
@@ -16,20 +52,20 @@
 </head>
 
 <body>
-      
+
 <div class="d-flex justify-content-center">
-	<div class="card">
+	<div class="card" style="margin: 50px">
 	<div class="card-body">
 	
 		<h1 class="text-center">Masuk</h1>	
-		<form>
-			<label class="card-title">Telepon</label><br>
-			<input class="form-control border-0 rounded-pill" style="background-color:#d1d1d1" type="number"/><br>
-			<label class="card-title">Kata Sandi</label><br>
-			<input class="form-control border-0 rounded-pill" style="background-color:#d1d1d1" type="password"/><br>
-			<a href="<?=url('Kode-otp')?>"><button class="btn login btn-block rounded-pill">Masuk</button></a>
+		<form action="" method="post" >
+			<label class="card-title">nama pengguna</label><br>
+			<input name="username" class="form-control border-0 rounded-pill" style="background-color:#d1d1d1" type="text"/><br>
+			<label class="card-title">kata sandi</label><br>
+			<input name="pass" class="form-control border-0 rounded-pill" style="background-color:#d1d1d1" type="password"/><br>
+			<input type="submit" name="kirim" value="Masuk" class="btn login btn-block rounded-pill" data-target="#exampleModalLong"> 
 			</br>
-			<span class="d-flex justify-content-center">Lupa Kata Sandi? <a href="<?=url('registrasi')?>" style="color:#30e64c;">Setel ulang kata sandi</a></span>
+			<span class="d-flex justify-content-center">Lupa Kata Sandi?</span>
 			</br>
 			
 			<div class="row d-flex justify-content-center">
@@ -49,7 +85,7 @@
 				<div class="col-sm">
 					<a class="btn btn-outline-dark btn-block rounded-pill" href="" role="button" style="text-transform:none">
 						<img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-					Masuk dengan Google
+					Masuk Dengan Google
 					</a>
 				</div>
 			</div>
@@ -59,14 +95,15 @@
 				<div class="col-sm">
 					<a class="btn btn-outline-dark btn-block rounded-pill" href="" role="button" style="text-transform:none">
 						<img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1365px-Facebook_f_logo_%282019%29.svg.png" />
-					Masuk dengan Facebook
+					Masuk Dengan Facebook
 					</a>
 				</div>
 			</div>
 			</br>
 
+
 			<p> Belum punya akun?
-			<a href="<?=url('registrasi')?>" style="color:#30e64c;">Daftar di sini</a>
+			<a href="index.php?halaman=daftar" style="color:#30e64c;">Register di sini</a>
 			</p>
 		</form>
 		
