8233101056c93aa36ef01f9be8217130096cd731
izzuddin3 <91045905+izzuddin3@users.noreply.github.com>
2021-10-16 08:35:54 +0700

    Update pengambilan.php

--- _halaman/pengambilan.php
+++ _halaman/pengambilan.php
@@ -1,8 +1,30 @@
 <?php
    $title="Pesan Pengambilan";
    $fileJS='pengambilan-js';
-?>
+   $connection = mysqli_connect("localhost","root","");
+   $db = mysqli_select_db($connection,'spst');
 
+   if(isset($_POST['insert'])) 
+   {
+     $sampah = $_POST ['sampah'];
+     $jumlah = $_POST ['jumlah'];
+
+     $query = "INSERT INTO jual (sampah,jumlah) VALUES ('$sampah','$jumlah') ";
+     $query_run = mysqli_query($connection,$query); 
+   
+   if($query_run)
+   {
+     echo ' <script type="text/javaScript"> alert("Data Tersimpan") 
+     </script>';
+   }
+
+   else
+   {
+     echo ' <script type="text/javaScript"> alert("Data Gagal Tersimpan") 
+     </script>';
+   }
+ }
+?>
 <div class="mb-2 align-items-center">
     <div class="card shadow mb-4">
          <div class="card-body">
@@ -16,40 +38,41 @@
                </div>
                <div class = ".col-sm">
                   <button type="button" class="btn mb-2 btn-outline-primary" onclick="getLocation()">Lokasi saat ini</button>
+                  
                </div>
              </div>
             <div id="mapid"></div>
             <div class = "row-sm">
-                <?=button_modal('Buat Pesanan', 'm_pesanan')?>
              </div>
-             <?=modal_select('m_pesanan', 'Masukkan Data Sampah', '
-             <div class="form-row">
-             <div class="form-group col-md-6">
-               <label for="simple-select2">sampah</label>
-               <select class="form-control select2" id="simple-select2">
-                 <optgroup label="Plastik">
-                   <option value="PET">PET</option>
+               <div class="container">
+               <form action="" method="POST">  
+               <label> Sampah </label><br>
+                 <select name="sampah">
+                 <option value="">- Plastik -</option>
+                 <option value="PET">PET</option>
                    <option value="HDPE">HDPE</option>
                    <option value="PVC">PVC</option>
                    <option value="LDPE">LDPE</option>
                    <option value="PP">PP</option>
                    <option value="PS">PS</option>
-                 </optgroup>
-                 <optgroup label="Kertas">
+      
+                 <option value="">- Kertas -</option>
                    <option value="HVS">HVS</option>
                    <option value="KRT">Karton</option>
                    <option value="KRD">Kardus</option>
                  </optgroup>
-                 <optgroup label="Organik">
+                 <option value="">- Organik -</option>
                  <option value="SB">Sampah Basah</option>
                  <option value="SK">Sampah Kering</option>
                </optgroup>
-               </select>
+               </select><br><br>
              </div> <!-- form-group -->
              <div class="form-group col-md-6">
-               <label>Jumlah (Kilograms)</label>
-               <input type="text" class="form-control" placeholder="berat sampah" aria-label="berat sampah">
-             </div>')?>
+               <label>Jumlah (Kilograms)</label><br>
+               <input type="jumlah" class="int" name="jumlah"> </div>
+               <input type="submit" class="txt" name="insert" value="Buat Pesanan"/>
+               
+              </div>
         </div>
     </div>
 </div>
