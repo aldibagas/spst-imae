a75535111cb2951cd28df4ae0a5ed4a76deb75bd
izzuddin3 <91045905+izzuddin3@users.noreply.github.com>
2021-10-16 08:24:22 +0700

    Update beranda.php

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -6,17 +6,30 @@
 <?=content_open('Jenis-Jenis Plastik')?>
       <p class="card-text">Informasi seputar jenis-jenis plastik dan bagaimana penggunaanya dalam lingkungan sehari-hari</p>
       <?=button_modal('Lihat','konten1')?>
-      <?=modal('konten1', 'Jenis-Jenis Plastik' , 'konten1')?>
+      <?=modal('konten1', 'Jenis-Jenis Plastik' , 'Plastik adalah komponen penting dan menjadi bahan baku dari banyak barang, contohnya seperti botol air, sisir, wadah minuman, peralatan makan, dan masih banyak lagi. Mengetahui perbedaan, jenis plastik, serta kode SPInya dapat membantu Anda dalam proses daur ulang sampah plastik.')?>
 <?=content_close()?>
 
 <?=content_open('Pentingnya lingkungan bersih')?>
       <p class="card-text">Kenapa kita harus menjaga agar lingkungan tetap bersih</p>
       <?=button_modal('Lihat','konten2')?>
-      <?=modal('konten2', 'Lingkungan bersih' , 'konten2')?>
+      <?=modal('konten2', 'Lingkungan bersih' , 'Kebersihan lingkungan merupakan keadaan bebas dari kotoran, termasuk di dalamnya, debu,
+sampah, dan bau. Di Indonesia, masalah kebersihan lingkungan selalu menjadi perdebatan
+dan masalah yang berkembang. Kasus-kasus yang menyangkut masalah kebersihan
+lingkungan setiap hari dan setiap tahun terus meningkat.
+Oleh karena itu menjaga kebersihan lingkungan sangatlah berguna untuk kita semua karena
+dapat menciptakan kehidupan yang aman, bersih, sejuk, dan sehat.
+Manfaat menjaga kebersihan lingkungan antara lain:
+1. Terhindar dari penyakit yang disebabkan lingkungan yang tidak sehat.
+2. Lingkungan menjadi lebih sejuk.
+3. Bebas dari polusi udara.
+4. Air menjadi lebih bersih dan aman untuk di minum.
+5. Lebih tenang dalam menjalankan aktivitas sehari hari.
+')?>
 <?=content_close()?>
 
 <?=content_open('Waspadai Genangan Air')?>
       <p class="card-text">Informasi seputar bahaya yang ditimbulkan dari genangan air</p>
       <?=button_modal('Lihat','konten3')?>
-      <?=modal('konten3', 'Lingkungan bersih' , 'konten3')?>
+      <?=modal('konten3', 'Lingkungan bersih' , 'Genangan air terdapat di tempat yang kotor. Genangan ini jika dibiarkan ada di sekitar rumah apalagi jika dilalui saat berjalan kaki akan menimbulkan bahaya kesehatan. Genangan air merupakan tempat berkembang biak nyamuk yang nantinya akan menjadi sumber penyakit yang ditularkan oleh nyamuk. 
+      Pastikan segera membersihkan segala benda atau bagian tubuh yang bersentuhan dengan air genangan. Menjaga kaki tetap kering juga penting. Kontak yang terlalu lama dengan genangan air dapat menyebabkan pengembangan parasit kaki, yaitu suatu kondisi yang dapat menyebabkan lepuh dan pembusukan jaringan')?>
 <?=content_close()?>
