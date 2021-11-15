5f575e8b401c1bc6515d591e5668f6648d01d4c8
aldibagas <aldibagas982@gmail.com>
2021-11-15 16:39:04 +0700

    Update env.php

--- env.php
+++ env.php
@@ -19,5 +19,5 @@ $route='js/leaflet-routing-machine/';
 $_SESSION['prefix']='workshop';
 
 //URI
-$setUri['base']='http://localhost/spst-imae/';
+$setUri['base']='http://spst.cypiral.org/';
 $setUri['assets']='assets/';
