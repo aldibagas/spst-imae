35728ae50db79de3b1e454c0ba14a6072c44b3ea
Farhan-zawahir <94586034+Farhan-zawahir@users.noreply.github.com>
2021-11-23 11:18:26 +0700

    Update penarikanuang.php

--- _halaman/penarikanuang.php
+++ _halaman/penarikanuang.php
@@ -1,5 +1,6 @@
 <?php
-   
+   session_start();
+   include '_helpers/connect.php';
    $title="Penarikan Uang";
    $ambil = mysqli_query($conn, "SELECT SUM(saldo) AS value_sum FROM tabungan WHERE kode=3");
    $row = mysqli_fetch_assoc($ambil);
@@ -7,7 +8,20 @@
 
    if(isset($_POST['kirim'])){
  
-    $pass = $_POST['pass'];
+     $idt = $_POST ['idt'];
+     $idp = $_POST ['idp'];
+     $idp2 = $_POST ['idp2'];
+     $aktivitas = $_POST ['aktivitas'];
+     $waktu_transaksi = $_POST ['waktu_transaksi'];
+     $biaya = $_POST['biaya'];
+     $metode_bayar= $_POST['metode_bayar'];
+     $metode_transaksi= $_POST['metode_transaksi'];
+     $status= $_POST['status'];
+     $pass = $_POST['pass'];
+
+     $query1 = "INSERT INTO `transaksi` ( `idt`, `idp`, `idp2`, `aktivitas`, `waktu_transaksi`, `biaya`, `metode_bayar`, `metode_transaksi`, `status`, `sandi`) 
+     VALUES ('1', '1', '1', '1', '$waktu_transaksi', '$biaya', '0', '0', '2','$pass')";
+     $query_run1 = mysqli_query($conn,$query1); 
 
     $sql = "SELECT * FROM `pengguna` WHERE `Sandi` = '$pass'";
     $run = mysqli_query($conn, $sql);
@@ -30,14 +44,15 @@
                                             <div class="form-group">
                                             </div>
                                             <div class="form-group">
-                                                <?php
+                                            <?php
                                             
                                             $tanggal = mktime(date('m'), date("d"), date('Y'));
                                             echo "Tanggal : <b> " . date("d-m-Y", $tanggal ) . "</b>";
                                             date_default_timezone_set("Asia/Jakarta");
                                             $jam = date ("H:i:s");
                                             echo " | Pukul : <b> " . $jam . " " ." </b> ";
-                                                ?>
+                                            ?>
+
                                             </div>
                                             <div class="form-group">
                                             <p class="mb-1 small text-muted">Saldo yang tersedia</p>
@@ -46,23 +61,27 @@
                                             </div>
                                             <div class="form-group">
                                             <p class="mb-1 small text-muted">Jumlah Saldo Yang ditarik</p>
-                                              <input class="form-control" name="jml" type="number" />
+                                            <input type="text" class="form-control" aria-label="saldo" name="biaya">
+
                                             </div>
                                             <div class="form-group">
                                             <p class="mb-1 small text-muted">Waktu Pengambilan Uang</p>
-                                                <input class="form-control" name="time" type="time" />
+                                            <div class="form-group mb-3">
+                                            <input type="time" class="form-control"  aria-label="saldo" name="waktu_transaksi">
+
                                             </div>
                                             <?=button_modal('Ajukan','confirm-pass')?>
                                             </div>
 <?=content_close()?>
 <?=modal_tanpa_button($id='confirm-pass', 'Konfirmasi Password', '
- <form action="" method="post">
- <label for="inputPassword" class="sr-only">Password</label>
- <label for="inputPassword" class="sr-only">Password</label>
- <input type="password" input name="pass" id="inputPassword" class="form-control form-control-lg" placeholder="Password" required="">
+<form action="" method="post">
+<label for="inputPassword" class="sr-only">Password</label>
+<label for="inputPassword" class="sr-only">Password</label>
+<input type="password" input name="pass" id="inputPassword" class="form-control form-control-lg" placeholder="Password" required="">
 </div>
 <div class="checkbox mb-3">
 </div>
 <input type="submit" name="kirim" value="Konfirmasi" button class="btn btn-lg btn-primary btn-lg" type="submit"></button>
 ')?>
-
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
