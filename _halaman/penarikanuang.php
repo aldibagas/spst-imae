fe7c6318f8f336adad48738712750a5087b6f8ba
Farhan-zawahir <94586034+Farhan-zawahir@users.noreply.github.com>
2021-11-23 14:57:14 +0700

    update penarikan

--- _halaman/penarikanuang.php
+++ _halaman/penarikanuang.php
@@ -2,7 +2,7 @@
    session_start();
    include '_helpers/connect.php';
    $title="Penarikan Uang";
-   $ambil = mysqli_query($conn, "SELECT SUM(saldo) AS value_sum FROM tabungan WHERE kode=3");
+   $ambil = mysqli_query($conn, "SELECT SUM(saldo) AS value_sum FROM tabungan WHERE idp=1");
    $row = mysqli_fetch_assoc($ambil);
    $saldo = $row['value_sum'];
 
@@ -12,24 +12,15 @@
      $idp = $_POST ['idp'];
      $idp2 = $_POST ['idp2'];
      $aktivitas = $_POST ['aktivitas'];
-<<<<<<< HEAD
      $waktu_transaksi = $_POST ['waktu_transaksi'];
-=======
-     $data_transaksi = $_POST ['data_transaksi'];
->>>>>>> 5c011b1d9b025395ed2a63ad859e145eb7117185
      $biaya = $_POST['biaya'];
      $metode_bayar= $_POST['metode_bayar'];
      $metode_transaksi= $_POST['metode_transaksi'];
      $status= $_POST['status'];
      $pass = $_POST['pass'];
 
-<<<<<<< HEAD
      $query1 = "INSERT INTO `transaksi` ( `idt`, `idp`, `idp2`, `aktivitas`, `waktu_transaksi`, `biaya`, `metode_bayar`, `metode_transaksi`, `status`, `sandi`) 
-     VALUES ('1', '1', '1', '1', '$waktu_transaksi', '$biaya', '0', '0', '2','$pass')";
-=======
-     $query1 = "INSERT INTO `transaksi` ( `idt`, `idp`, `idp2`, `aktivitas`, `data_transaksi`, `biaya`, `metode_bayar`, `metode_transaksi`, `status`, `sandi`) 
-     VALUES ('1', '3', '3', '1', '$data_transaksi', '$biaya', '0', '0', '2','$pass')";
->>>>>>> 5c011b1d9b025395ed2a63ad859e145eb7117185
+     VALUES ('3', '2', '3', '1', '$waktu_transaksi', '$biaya', '0', '0', '2','$pass')";
      $query_run1 = mysqli_query($conn,$query1); 
 
     $sql = "SELECT * FROM `pengguna` WHERE `Sandi` = '$pass'";
@@ -76,13 +67,8 @@
                                             <div class="form-group">
                                             <p class="mb-1 small text-muted">Waktu Pengambilan Uang</p>
                                             <div class="form-group mb-3">
-<<<<<<< HEAD
                                             <input type="time" class="form-control"  aria-label="saldo" name="waktu_transaksi">
 
-=======
-                                            <label for="">Time</label>
-                                            <input type="time" name="time" class="form-control">
->>>>>>> 5c011b1d9b025395ed2a63ad859e145eb7117185
                                             </div>
                                             <?=button_modal('Ajukan','confirm-pass')?>
                                             </div>
@@ -97,8 +83,3 @@
 </div>
 <input type="submit" name="kirim" value="Konfirmasi" button class="btn btn-lg btn-primary btn-lg" type="submit"></button>
 ')?>
-<<<<<<< HEAD
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-=======
->>>>>>> 5c011b1d9b025395ed2a63ad859e145eb7117185
