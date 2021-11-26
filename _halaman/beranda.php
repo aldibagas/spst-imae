f6836337ba424b4a4233f1e22137073748bacbee
izzuddin3 <91045905+izzuddin3@users.noreply.github.com>
2021-11-26 15:50:19 +0700

    Update beranda.php

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -13,81 +13,22 @@
    
    $ambil = mysqli_query($conn, "SELECT * FROM tabungan WHERE idp1='$id'");
    $row = mysqli_fetch_assoc($ambil);
-   $saldo=0;
+   $saldo=$row['saldo'];
    if($row['saldo']<=0){
     $saldo = 0;
    }else{
     $saldo = $row['saldo'];
    }
 
-
-   if(isset($_POST['kirim'])){
-    
-    $sqlId = "select * from pengguna where Nama = '$nama'";
-    $idRun = mysqli_query($conn, $sqlId);
-    $ambilId = mysqli_fetch_assoc($idRun);
-    $id  = $ambilId['idp'];
-
-     //$idt = $_POST ['idt'];
-     //$idp1 = $_POST ['idp1'];
-     $idp2 = $id;
-     @$aktivitas = $_POST ['aktivitas'];
-     $waktu_tarik = $_POST ['waktu_tarik'];
-     $jumlah_tarik = $_POST['jumlah_tarik'];
-     @$metode_bayar= $_POST['metode_bayar'];
-     @$metode_transaksi= $_POST['metode_transaksi'];
-     @$status= $_POST['status'];
-     $pass = $_POST['pass'];
-
-     //$sql1 ="INSERT INTO `transaksi` ( `idt`, `idp1`, `idp2`, `aktivitas`, `waktu_tarik`, `jumlah_tarik`, `metode_bayar`, `metode_transaksi`, `status_tarik`, `sandi`) 
-     //VALUES ('4', '2', '3', '1', '$waktu_tarik', '$jumlah_tarik', '0', '0', '0','$pass')";
-
-    //kirim notifikasi pada petugas secara acak
-    //pemilihan petugas
-    $sqlRand = "select idp from pengguna where kelas = 'petugas'";
-    $runRand = mysqli_query($conn, $sqlRand);
-    $idPetugasRand = array();
-    while($fchRand = mysqli_fetch_assoc($runRand)){
-        $idPetugasRand[] = $fchRand['idp'];
-    }
-    $rand = array_rand($idPetugasRand);
-    $idPetugas = $idPetugasRand[$rand];
-
-    //pengiriman notifikasi
-     $sqlnotif = "INSERT INTO `notifikasi` (`idt`,`idp2`, `idpetugas`,`waktu_tarik`,`jumlah_tarik`,`metode_bayar`, `metode_transaksi`,`status_tarik`)
-     VALUES ('', '$id', '$idPetugas','$waktu_tarik', '$jumlah_tarik', '0', '0', null)";
-
-    //ambil
-    $sqlAmbl = "select * from tabungan where idp1 = '$idp2'";
-    $runAmbil = mysqli_query($conn, $sqlAmbl);
-    $ambilSaldo = mysqli_fetch_assoc($runAmbil);
-
-    //pengurangan
-    $saldoAkhir = $ambilSaldo['saldo'] - $jumlah_tarik;
-
-    $sqlTabungan = "UPDATE `tabungan` SET `tanggal`='$waktu_tarik',`saldo`='$saldoAkhir' WHERE `idp1`='$id'";
-
-     //$query2 = mysqli_query($conn,$sql1); 
-
-     mysqli_commit($conn);
-
-     $sql = "SELECT * FROM `pengguna` WHERE `Sandi` = '$pass'";
-     $run = mysqli_query($conn, $sql);
-     $row = mysqli_fetch_assoc($run);
-
-    if($row > 0){
-        //jika password benar
-        //update
-        mysqli_query($conn, $sqlTabungan);
-        mysqli_query($conn,$sqlnotif);
-        //header('location:index.php?halaman=transaksi');
-    }else{
-        echo'
-        <div style="width:100%;color:white;background-color:red;text-align:center;padding:5px;font-weight:bold;">Password Salah</div>"
-        ';
-    }
-}
-
+   $nama = $_SESSION['nama'];
+   $sql ="SELECT * FROM `pengguna` WHERE Nama = '$nama'";
+   $Query = "SELECT * FROM 'pemesanan' WHERE idp=1";
+   $run = mysqli_query($conn, $sql);
+   $row = mysqli_fetch_assoc($run);
+   $email = $row['email'];
+   $telp = $row['Telepon'];
+   $alamat = $row['alamat'];
+ 
    $Cari="SELECT * FROM pemesanan order by tanggal desc LIMIT 5";
    $Tampil = mysqli_query($conn, $Cari);
    $data = array();
@@ -138,7 +79,7 @@
                     <div class="row mt-2">
                       <div class="col-6 text-center mb-3 border-right">
                         <p class="text-muted mb-1">PETE</p>
-                        <h6 class="mb-1">Rp 300</h6>
+                        <h6 class="mb-1">- Rp 300</h6>
                         <p class="text-muted mb-2">/kg</p>
                       </div>
                       <div class="col-6 text-center mb-3">
