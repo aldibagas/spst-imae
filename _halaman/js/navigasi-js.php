39f789ff65011259814aee1933c9beb5c1a057d2
izzuddin3 <91045905+izzuddin3@users.noreply.github.com>
2021-12-04 18:33:38 +0700

    updatenavigasi

--- _halaman/js/navigasi-js.php
+++ _halaman/js/navigasi-js.php
@@ -16,6 +16,7 @@
 	});
 	
 	map.addLayer(Layer);
+
 	// ambil titik
 	getLocation();
 	setInterval(() => {
--- _halaman/navigasi.php
+++ _halaman/navigasi.php
@@ -3,13 +3,14 @@
    $fileJS='navigasi-js';
    $Template=true;
 
-   $Cari="SELECT * FROM navigasi where idt = 7";
+   $Cari="SELECT * FROM navigasi where idt = 1";
    $Tampil = mysqli_query($conn, $Cari);
    $row = mysqli_fetch_assoc($Tampil);
    echo "<div id='lat' style='visibility:hidden'>".$row['latitude']."</div>
          <div id='long' style='visibility:hidden'>".$row['longitude']."</div>
    ";
 ?>
+
 <div class="mb-2 align-items-center">
     <div class="card shadow mb-4">
         <div class="card-body">
