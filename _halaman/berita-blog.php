284298a96e5c9f2b0e992d95dc90339fc87a692b
aldibagas <aldibagas982@gmail.com>
2021-12-23 14:19:28 +0700

    Create berita-blog.php

---
+++ _halaman/berita-blog.php
@@ -0,0 +1,113 @@
+<?php
+$judul = "";
+$gambar1="";
+$gambar2="";
+$paragraf1="";
+$paragraf2="";
+$paragraf3="";
+?>
+
+<section class="section blog-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="row">
+	<div class="col-lg-12 mb-5">
+		<div class="single-blog-item">
+			<img src="images/blog/blog-1.jpg" alt="" class="img-fluid">
+
+			<div class="blog-item-content mt-5">
+
+				<h2 class="mb-4 text-md"><?=$judul?></h2>
+
+				<p class="lead mb-4"><?=$paragraf1?></p>
+
+				<p><?=$paragraf2?></p>
+
+				<p><?$paragraf3?>s</p>
+
+				<div class="mt-5 clearfix">
+				    <ul class="float-left list-inline tag-option"> 
+				    	<li class="list-inline-item"><a href="#">Advancher</a></li>
+				    	<li class="list-inline-item"><a href="#">Landscape</a></li>
+				    	<li class="list-inline-item"><a href="#">Travel</a></li>
+				   	</ul>        
+
+				    <ul class="float-right list-inline">
+				        <li class="list-inline-item"> Share: </li>
+				        <li class="list-inline-item"><a href="#" target="_blank"><i class="icofont-facebook" aria-hidden="true"></i></a></li>
+				        <li class="list-inline-item"><a href="#" target="_blank"><i class="icofont-twitter" aria-hidden="true"></i></a></li>
+				        <li class="list-inline-item"><a href="#" target="_blank"><i class="icofont-pinterest" aria-hidden="true"></i></a></li>
+				        <li class="list-inline-item"><a href="#" target="_blank"><i class="icofont-linkedin" aria-hidden="true"></i></a></li>
+				    </ul>
+			    </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-12">
+		<div class="comment-area mt-4 mb-5">
+			<h4 class="mb-4">2 Comments on Healthy environment... </h4>
+			<ul class="comment-tree list-unstyled">
+				<li class="mb-5">
+					<div class="comment-area-box">
+						<div class="comment-thumb float-left">
+							<img alt="" src="images/blog/testimonial1.jpg" class="img-fluid">
+						</div>
+
+						<div class="comment-info">
+							<h5 class="mb-1">John</h5>
+							<span>United Kingdom</span>
+							<span class="date-comm">| Posted April 7, 2019</span>
+						</div>
+						<div class="comment-meta mt-2">
+							<a href="#"><i class="icofont-reply mr-2 text-muted"></i>Reply</a>
+						</div>
+
+						<div class="comment-content mt-3">
+							<p>Some consultants are employed indirectly by the client via a consultancy staffing company, a company that provides consultants on an agency basis. </p>
+						</div>
+					</div>
+				</li>
+
+				<li>
+					<div class="comment-area-box">
+						<div class="comment-thumb float-left">
+							<img alt="" src="images/blog/testimonial2.jpg" class="img-fluid">
+						</div>
+
+						<div class="comment-info">
+							<h5 class="mb-1">Philip W</h5>
+							<span>United Kingdom</span>
+							<span class="date-comm">| Posted June 7, 2019</span>
+						</div>
+
+						<div class="comment-meta mt-2">
+							<a href="#"><i class="icofont-reply mr-2 text-muted"></i>Reply </a>
+						</div>
+
+						<div class="comment-content mt-3">
+							<p>Some consultants are employed indirectly by the client via a consultancy staffing company, a company that provides consultants on an agency basis. </p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+            </div>
+            <div class="col-lg-4">
+                <div class="sidebar-wrap pl-lg-4 mt-5 mt-lg-0">
+	<div class="sidebar-widget search  mb-3 ">
+		<h5>Search Here</h5>
+		<form action="#" class="search-form">
+			<input type="text" class="form-control" placeholder="search">
+			<i class="ti-search"></i>
+		</form>
+	</div>
+
+</div>
+            </div>   
+        </div>
+    </div>
+</section>
