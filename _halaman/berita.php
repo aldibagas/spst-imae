288c9b2c188b5dd028b7e1293f3328c81adcc7e9
aldibagas <aldibagas982@gmail.com>
2021-12-23 13:39:12 +0700

    berita

---
+++ _halaman/berita.php
@@ -0,0 +1,21 @@
+<?php
+// session_start();
+// if($_SESSION['id'] == null){
+//     header('Location:index.php?halaman=login');
+// }
+?>
+
+<section class="page-title bg-1">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="block text-center">
+          <span class="text-white">News details</span>
+          <h1 class="text-capitalize mb-5 text-lg">Blog Single</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
